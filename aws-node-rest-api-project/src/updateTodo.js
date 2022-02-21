34760b0b098c0fa748ee00ff8f0f43139ac2316c
Saúl López Ochoa <82007089+izcadiz@users.noreply.github.com>
2022-02-21 16:39:45 -0600

    cursos AWS

---
+++ aws-node-rest-api-project/src/updateTodo.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const { v4 } = require("uuid")
+const AWS = require("aws-sdk")
+const middy = require("@middy/core")
+const httpJsonBodyParse = require("@middy/http-json-body-parser")
+
+const updateTodo = async (event) => {
+
+  const dynamodb = new AWS.DynamoDB.DocumentClient()
+  const { completed } = event.body;
+  const { id } = event.pathParameters
+
+
+  await dynamodb.update({
+    TableName: "TodoTable",
+    Key: { id },
+    UpdateExpression: 'set completed = :completed',
+    ExpressionAttributeValues: {
+    ':completed': completed
+    },
+    ReturnValues: "ALL_NEW"
+    }).promise()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+    msg: "Todo Updated"
+    }),
+  };
+};
+
+module.exports = {
+    handler: middy(updateTodo).use(httpJsonBodyParse())
+}
